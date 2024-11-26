b1db33ec13fa19d98026033bca8b79a130b501c5
Mamun-sharmin <mamun991646@gmail.com>
2024-11-26 19:44:29 +0600

    Layout.tsx Updated with font, Title and body class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,25 @@
 import type { Metadata } from "next";
-import "./globals.css";
 
+//FONT IMPORT
+import {Inter, Calistoga} from 'next/font/google'
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+//FONT CUSTOMIZATION
+const inter= Inter({ 
+  subsets: ['latin'],
+   variable: '--font-sans'
+  });
+const calistoga= Calistoga ({
+   subsets: ['latin'],
+    variable: '--font-serif', 
+    weight:["400"],
+  })
+
+  // TITLE AND DESCRIPTION
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: "Fahriar Ahmed",
+  description: "We develop various types of Ecommerce website, Mobile app and Portfolio. You are most welcome with me and my team",
 };
 
 export default function RootLayout({
@@ -13,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={twMerge("bg-gray-900 text-white antialiased")}>{children}</body>
     </html>
   );
 }
