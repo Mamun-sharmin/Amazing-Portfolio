d6b7d75fe00fd3d691bae4dd801473b4e23bce5b
Mamun-sharmin <mamun991646@gmail.com>
2024-11-27 07:55:41 +0600

    Header section completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,29 @@
 import type { Metadata } from "next";
-import "./globals.css";
 
+//FONT IMPORT
+import {Inter, Calistoga} from 'next/font/google'
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+//FONT CUSTOMIZATION
+const inter= Inter({ 
+  subsets: ['latin'],
+   variable: '--font-sans'
+  });
+const calistoga= Calistoga ({
+   subsets: ['latin'],
+    variable: '--font-serif', 
+    weight:["400"],
+  })
+
+  // TITLE AND DESCRIPTION
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: "Fahriar Ahmed",
+  description: "We develop various types of Ecommerce website, Mobile app and Portfolio. You are most welcome with me and my team",
 };
 
+
+//HTML
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -13,7 +31,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body 
+        className={twMerge(
+        inter.variable, 
+        calistoga.variable,
+        "bg-gray-900 text-white antialiased font-sans"
+        )}>
+          {children}
+      </body>
     </html>
   );
 }
